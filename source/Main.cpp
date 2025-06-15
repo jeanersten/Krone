9abc82541e42f3784ad5c8a7e21fbb2bc2be28cc
Ahmad Fauzan <gavier.exe@gmail.com>
2025-06-15 21:15:45 +0700

    ini: GLAD library

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,3 +1,4 @@
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -7,6 +8,22 @@ int main()
   if (glfwInit())
   {
     std::cout << "GLFW initialized successfully\n";
+  }
+
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Krone Engine!", nullptr, nullptr);
+
+  if (!window)
+  {
+    std::cerr << "Failed to create GLFW window!\n";
+  }
+  else
+  {
+    glfwMakeContextCurrent(window);
+  }
+
+  if (gladLoadGL(glfwGetProcAddress))
+  {
+    std::cout << "OpenGL functions retrived via GLAD!\n";
   }
   
   std::cout << "Welcome to Krone Engine!\n";
