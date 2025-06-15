86319edb229e9a0a9cadc449b412c79b3c47e8b8
Ahmad Fauzan <gavier.exe@gmail.com>
2025-06-15 18:19:59 +0700

    ini: GLFW library

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,7 +1,14 @@
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main()
 {
+  if (glfwInit())
+  {
+    std::cout << "GLFW initialized successfully\n";
+  }
+  
   std::cout << "Welcome to Krone Engine!\n";
 
   return 0;
